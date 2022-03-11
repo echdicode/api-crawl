13a8af377182f89fd5b04bd580b1c5ed23c27921
thangdd <ducthangpg231@gmail.com>
2022-03-11 16:42:08 +0700

    make it better

---
+++ app.py
@@ -0,0 +1,21 @@
+import sys
+import json
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/kenh14', methods=['GET'])
+def verify():
+    pages = 'https://kenh14.vn/sport.html'
+    page = requests.get(pages)
+    print(page.content)
+    web_page = page.content
+    soup = BeautifulSoup(web_page, "html.parser")
+    test = soup.findAll(class_='knswli need-get-value-facebook clearfix')
+    links = [link.find('a').attrs["href"] for link in test]
+    return links
+def log(message):  # simple wrapper for logging to stdout on heroku
+    print(message)
+    sys.stdout.flush()
+
